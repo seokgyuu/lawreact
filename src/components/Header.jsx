2b9b0302d3ea8539d0f8ed07b55a0c3e338b782c
gyu <tommyne99@naver.com>
2024-10-10 17:49:40 +0900

    10/10 header 링크 url 설정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,54 @@
 import React, { useState } from "react";
+import {useLocation} from "react-router-dom";
 import { CommentOutlined, AppstoreOutlined, MenuOutlined } from "@ant-design/icons";
 
-const headerNav = [
-    {
-        title: "ChatBot",
-        url: "#Comant1",
-        icon: CommentOutlined
-    },
-    {
-        title: "기능",
-        url: "#Comant",
-        icon: AppstoreOutlined
-    }
-];
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const location = useLocation();
+    
+    let headerNav;
+    if (location.pathname === "/home") {
+        headerNav = [
+            {
+                title: "ChatBot1",
+                url: "/",
+                icon: CommentOutlined
+            },
+            {
+                title: "기능",
+                url: "/",
+                icon: AppstoreOutlined
+            }
+        ];
+   
+}  else if(location.pathname === "/home2"){
+    headerNav =[
+        {
+            title: "ChatBot2",
+            url: "/",
+            icon: CommentOutlined
+        },
+        {
+            title: "기능",
+            url: "/",
+            icon: AppstoreOutlined
+        }
+    ];
+}else if(location.pathname === "/cal"){
+    headerNav =[
+        {
+            title: "ChatBot3",
+            url: "/",
+            icon: CommentOutlined
+        },
+        {
+            title: "기능",
+            url: "/",
+            icon: AppstoreOutlined
+        }
+    ];
+}
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -25,7 +58,7 @@ const Header = () => {
         <header className="header">
             <div className="header__inner">
                 <div className="header__logo">
-                    <a href="/home">
+                    <a href="/">
                         <h1>team2</h1>
                     </a>
                 </div>
